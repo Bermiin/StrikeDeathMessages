4709ffd587478350b1ea2a5c84149e12a866ca31
Bermine <bermine@seznam.cz>
2022-09-11 19:02:24 +0200

    Fixed config paths

--- src/main/java/me/bermine/sdm/listeners/StrikeListeners.java
+++ src/main/java/me/bermine/sdm/listeners/StrikeListeners.java
@@ -18,7 +18,7 @@ public class StrikeListeners implements Listener {
     @EventHandler
     public void onDuelEnd(DuelEndEvent e) {
         FileConfiguration config = StrikeDeathMessages.getInstance().getConfig();
-        if (!config.getBoolean("death_message.enabled")) return;
+        if (!config.getBoolean("death.enabled")) return;
         String winner = e.getWinner().getName();
         String looser = e.getLoser().getName();
         if (config.getBoolean("death.sound.enabled")) {
@@ -26,7 +26,7 @@ public class StrikeListeners implements Listener {
             e.getWinner().playSound(e.getLoser().getLocation(), Sound.valueOf(config.getString("death.sound.sound")), (float) config.getInt("death.sound.volume"), (float) config.getInt("death.sound.pitch"));
         }
         e.getFight().getPlayersInFight().forEach(p ->
-            p.sendMessage(Color.translate(config.getString("death_message.format")
+            p.sendMessage(Color.translate(config.getString("death.message")
                     .replace("<looser>", looser)
                     .replace("<winner>", winner)
             ))
@@ -36,9 +36,9 @@ public class StrikeListeners implements Listener {
     @EventHandler
     public void onDuelStart(DuelStartEvent e) {
         FileConfiguration config = StrikeDeathMessages.getInstance().getConfig();
-        if (!config.getBoolean("start_message.enabled")) return;
+        if (!config.getBoolean("start.enabled")) return;
         Bukkit.getScheduler().runTaskLater(StrikeDeathMessages.getInstance(), () ->
-            config.getStringList("start_message.format").stream().map(Color::translate).forEach(s -> {
+            config.getStringList("start.message").stream().map(Color::translate).forEach(s -> {
                 e.getPlayer1().sendMessage(s);
                 e.getPlayer2().sendMessage(s);
             }),101L);

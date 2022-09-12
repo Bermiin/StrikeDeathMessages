f7c20ae4d2011b67f21173d07cec25e8e148235a
Bermine <bermine@seznam.cz>
2022-09-12 23:15:11 +0200

    Fixed NPE when there is no winner, added win messages to bridge & bedwars gamemode, refactoring

--- src/main/java/me/bermine/sdm/listeners/StrikeListeners.java
+++ src/main/java/me/bermine/sdm/listeners/StrikeListeners.java
@@ -4,10 +4,9 @@ import ga.strikepractice.events.DuelEndEvent;
 import ga.strikepractice.events.DuelStartEvent;
 import ga.strikepractice.fights.Fight;
 import me.bermine.sdm.StrikeDeathMessages;
-import me.bermine.sdm.util.Color;
+import me.bermine.sdm.util.ConfigUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -18,33 +17,42 @@ public class StrikeListeners implements Listener {
 
     @EventHandler
     public void onDuelEnd(DuelEndEvent event) {
-        FileConfiguration config = StrikeDeathMessages.getInstance().getConfig();
-        if (!config.getBoolean("death.enabled")) return;
+        if (!ConfigUtils.DEATH_ENABLED) return;
+        if (event.getWinner() == null || event.getLoser() == null) return;
         String winner = event.getWinner().getName();
         String loser = event.getLoser().getName();
-        if (config.getBoolean("death.sound.enabled")) {
-            event.getLoser().playSound(event.getLoser().getLocation(), Sound.valueOf(config.getString("death.sound.sound")), (float) config.getInt("death.sound.volume"), (float) config.getInt("death.sound.pitch"));
-            event.getWinner().playSound(event.getLoser().getLocation(), Sound.valueOf(config.getString("death.sound.sound")), (float) config.getInt("death.sound.volume"), (float) config.getInt("death.sound.pitch"));
+        if (ConfigUtils.DEATH_SOUND_ENABLED) {
+            event.getLoser().playSound(event.getLoser().getLocation(), Sound.valueOf(ConfigUtils.DEATH_SOUND), ConfigUtils.DEATH_SOUND_VOLUME, ConfigUtils.DEATH_SOUND_PITCH);
+            event.getWinner().playSound(event.getLoser().getLocation(), Sound.valueOf(ConfigUtils.DEATH_SOUND), ConfigUtils.DEATH_SOUND_VOLUME, ConfigUtils.DEATH_SOUND_PITCH);
         }
         Fight fight = event.getFight();
         if(fight == null) {
             return;
         }
-        String message = Color.translate(config.getString("death.message")
+
+        if (fight.getKit().isBridges() || fight.getKit().isBedwars()) {
+            String message = ConfigUtils.DEATH_WIN
+                    .replace("<looser>", loser)
+                    .replace("<winner>", winner);
+            fight.getPlayersInFight().forEach(player -> player.sendMessage(message));
+            fight.getSpectators().forEach(player -> player.sendMessage(message));
+            return;
+        }
+
+        String message = ConfigUtils.DEATH_MSG
                         .replace("<looser>", loser)
-                        .replace("<winner>", winner));
+                        .replace("<winner>", winner);
         fight.getPlayersInFight().forEach(player -> player.sendMessage(message));
         fight.getSpectators().forEach(player -> player.sendMessage(message));
     }
 
     @EventHandler
     public void onDuelStart(DuelStartEvent e) {
-        FileConfiguration config = StrikeDeathMessages.getInstance().getConfig();
-        if (!config.getBoolean("start.enabled")) return;
+        if (!ConfigUtils.START_ENABLED) return;
         Bukkit.getScheduler().runTaskLater(StrikeDeathMessages.getInstance(), () ->
-            config.getStringList("start.message").stream().map(Color::translate).forEach(s -> {
+            ConfigUtils.START_MSG.forEach(s -> {
                 e.getPlayer1().sendMessage(s);
                 e.getPlayer2().sendMessage(s);
-            }),101L);
+        }),101L);
     }
 }
